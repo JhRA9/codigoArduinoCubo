fc1cb812923a564c22f4ec32d8d52cd25092650a
JhRA9 <jhonysperez21@gmail.com>
2026-05-05 09:46:26 -0500

    Correcion documentacion

--- codigo-Arduino/Simulacion_proyecto/Simulacion_proyecto.ino
+++ codigo-Arduino/Simulacion_proyecto/Simulacion_proyecto.ino
@@ -3,84 +3,111 @@
 // pin de datos
 #define LED_PIN 6
 
-// total leds
+// total de leds
 #define NUM_LEDS 512
 
-// brillo general
+// brillo
 #define BRIGHTNESS 50
 
-// arreglo de leds
-CRGB leds[NUM_LEDS];
-
-// tamano del cubo
+// tamaño del cubo
 #define SIZE 8
 
-// convierte x,y,z a indice
+CRGB leds[NUM_LEDS];
+
+// matriz logica 3D
+int cube[SIZE][SIZE][SIZE];
+
+// convierte coordenadas a indice
 int getIndex(int x, int y, int z) {
   return z * SIZE * SIZE + y * SIZE + x;
 }
 
-// enciende un led
-void setLED(int x, int y, int z, CRGB color) {
-  if (x >= 0 && x < SIZE && y >= 0 && y < SIZE && z >= 0 && z < SIZE) {
-    leds[getIndex(x,y,z)] = color;
+// limpia matriz logica
+void clearMatrix() {
+  for (int x = 0; x < SIZE; x++)
+    for (int y = 0; y < SIZE; y++)
+      for (int z = 0; z < SIZE; z++)
+        cube[x][y][z] = 0;
+}
+
+// renderiza matriz a leds
+void renderCube(CRGB color) {
+  for (int x = 0; x < SIZE; x++) {
+    for (int y = 0; y < SIZE; y++) {
+      for (int z = 0; z < SIZE; z++) {
+
+        if (cube[x][y][z] == 1)
+          leds[getIndex(x, y, z)] = color;
+        else
+          leds[getIndex(x, y, z)] = CRGB::Black;
+
+      }
+    }
   }
+  FastLED.show();
+}
+
+// enciende voxel logico
+void setVoxel(int x, int y, int z) {
+  if (x >= 0 && x < SIZE && y >= 0 && y < SIZE && z >= 0 && z < SIZE)
+    cube[x][y][z] = 1;
 }
 
 // apaga todo
 void clearCube() {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Black;
-  }
-  FastLED.show();
+  clearMatrix();
+  renderCube(CRGB::Black);
 }
 
-// plano XY en altura z
-void planoXY(int z, CRGB color) {
-  clearCube();
+// plano XY
+void planoXY(int z) {
+  clearMatrix();
 
   for (int x = 0; x < SIZE; x++)
     for (int y = 0; y < SIZE; y++)
-      setLED(x, y, z, color);
+      setVoxel(x, y, z);
 
-  FastLED.show();
+  renderCube(CRGB::Blue);
 }
 
-// plano XZ en fila y
-void planoXZ(int y, CRGB color) {
-  clearCube();
+// plano XZ
+void planoXZ(int y) {
+  clearMatrix();
 
   for (int x = 0; x < SIZE; x++)
     for (int z = 0; z < SIZE; z++)
-      setLED(x, y, z, color);
+      setVoxel(x, y, z);
 
-  FastLED.show();
+  renderCube(CRGB::Red);
 }
 
-// plano YZ en columna x
-void planoYZ(int x, CRGB color) {
-  clearCube();
+// plano YZ
+void planoYZ(int x) {
+  clearMatrix();
 
   for (int y = 0; y < SIZE; y++)
     for (int z = 0; z < SIZE; z++)
-      setLED(x, y, z, color);
+      setVoxel(x, y, z);
 
-  FastLED.show();
+  renderCube(CRGB::Green);
 }
 
-// enciende todo el cubo
-void cuboCompleto(CRGB color) {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = color;
-  }
+// cubo completo
+void cuboCompleto() {
+  clearMatrix();
 
-  FastLED.show();
+  for (int x = 0; x < SIZE; x++)
+    for (int y = 0; y < SIZE; y++)
+      for (int z = 0; z < SIZE; z++)
+        setVoxel(x, y, z);
+
+  renderCube(CRGB::White);
 }
 
-// barrido en eje z
+// barrido en Z
 void barridoXY() {
   for (int z = 0; z < SIZE; z++) {
-    planoXY(z, CRGB::Blue);
+    planoXY(z);
     delay(150);
   }
 }
@@ -89,30 +116,46 @@ void barridoXY() {
 void expansionCentro() {
   int c = SIZE / 2;
 
-  for (int r = 0; r < c; r++) {
-    clearCube();
+  for (int r = 0; r <= c; r++) {
+    clearMatrix();
 
     for (int x = c - r; x <= c + r; x++)
       for (int y = c - r; y <= c + r; y++)
         for (int z = c - r; z <= c + r; z++)
-          setLED(x, y, z, CRGB::Green);
+          setVoxel(x, y, z);
 
-    FastLED.show();
+    renderCube(CRGB::Purple);
     delay(200);
   }
 }
 
-// rotacion con planos
+// rotacion de planos
 void rotacionPlanos() {
   for (int i = 0; i < SIZE; i++) {
-    planoXY(i, CRGB::Red);
+    planoXY(i);
     delay(100);
 
-    planoYZ(i, CRGB::Yellow);
+    planoYZ(i);
     delay(100);
 
-    planoXZ(i, CRGB::Purple);
+    planoXZ(i);
     delay(100);
+  }
+}
+
+// animacion tipo lluvia
+void lluvia() {
+  for (int i = 0; i < 30; i++) {
+
+    int x = random(0, SIZE);
+    int y = random(0, SIZE);
+
+    for (int z = SIZE - 1; z >= 0; z--) {
+      clearMatrix();
+      setVoxel(x, y, z);
+      renderCube(CRGB::Cyan);
+      delay(60);
+    }
   }
 }
 
@@ -135,6 +178,12 @@ void loop() {
 
   rotacionPlanos();
   delay(300);
+
+  lluvia();
+  delay(300);
+
+  cuboCompleto();
+  delay(500);
 
   clearCube();
   delay(500);
